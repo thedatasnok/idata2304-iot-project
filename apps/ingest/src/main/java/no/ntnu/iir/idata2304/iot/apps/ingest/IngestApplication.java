428e554d95651d92c36ea989782d18865c4f798a
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-10-18 08:27:06 +0200

    build: scaffold gradle project

---
+++ apps/ingest/src/main/java/no/ntnu/iir/idata2304/iot/apps/ingest/IngestApplication.java
@@ -0,0 +1,9 @@
+package no.ntnu.iir.idata2304.iot.apps.ingest;
+
+public class IngestApplication {
+  
+  public static void main(String[] args) {
+    // implement me
+  }
+
+}
---
+++ apps/sensor/src/main/java/no/ntnu/iir/idata2304/iot/apps/sensor/SensorApplication.java
@@ -0,0 +1,9 @@
+package no.ntnu.iir.idata2304.iot.apps.sensor;
+
+public class SensorApplication {
+  
+  public static void main(String[] args) {
+    // implement me
+  }
+
+}

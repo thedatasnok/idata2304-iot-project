0a84a96128f9667b7523b61df039a36e0de27196
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-10-25 17:24:00 +0200

    feat: #16 set up base spring applications

--- apps/ingest/src/main/java/no/ntnu/iir/idata2304/iot/apps/ingest/IngestApplication.java
+++ apps/ingest/src/main/java/no/ntnu/iir/idata2304/iot/apps/ingest/IngestApplication.java
@@ -1,9 +1,13 @@
 package no.ntnu.iir.idata2304.iot.apps.ingest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class IngestApplication {
   
   public static void main(String[] args) {
-    // implement me
+    SpringApplication.run(IngestApplication.class, args);
   }
 
 }
--- apps/sensor/src/main/java/no/ntnu/iir/idata2304/iot/apps/sensor/SensorApplication.java
+++ apps/sensor/src/main/java/no/ntnu/iir/idata2304/iot/apps/sensor/SensorApplication.java
@@ -1,9 +1,13 @@
 package no.ntnu.iir.idata2304.iot.apps.sensor;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SensorApplication {
   
   public static void main(String[] args) {
-    // implement me
+    SpringApplication.run(SensorApplication.class, args);
   }
 
 }

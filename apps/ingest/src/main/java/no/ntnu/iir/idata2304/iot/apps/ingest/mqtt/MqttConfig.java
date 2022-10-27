a9caf53ca835db2f672b0bd8fa3b4d4eba2f6ab9
Dawid Staniszewski <dstaniszewski00@gmail.com>
2022-10-27 14:55:59 +0200

    feat(ingest): #11 Set up configuration for receiving MQTT messages

---
+++ apps/ingest/src/main/java/no/ntnu/iir/idata2304/iot/apps/ingest/mqtt/MqttConfig.java
@@ -0,0 +1,51 @@
+package no.ntnu.iir.idata2304.iot.apps.ingest.mqtt;
+
+import java.net.InetAddress;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+
+@Configuration
+public class MqttConfig {
+
+  @Value("${ingest.mqtt.broker.ip}")
+  private InetAddress ipAddress;
+
+  @Value("${ingest.mqtt.broker.port}")
+  private String port;
+
+  @Value("${ingest.mqtt.client.id}")
+  private String clientId;
+
+  private static final String TOPIC = "g9hood/+/+/cpu/group09/+";
+
+  private static final int TIMEOUT = 5000;
+
+  public MessageChannel mqttInputChannel() {
+    return new DirectChannel();
+  }
+
+  public MessageProducer inbound() {
+    var adapter = new MqttPahoMessageDrivenChannelAdapter(
+        this.buildMqttServerUri(), 
+        this.clientId, 
+        TOPIC
+    );
+
+    adapter.setCompletionTimeout(TIMEOUT);
+    adapter.setConverter(new DefaultPahoMessageConverter());
+    adapter.setQos(1);
+    adapter.setOutputChannel(mqttInputChannel());
+
+    return adapter;
+  }
+
+  private String buildMqttServerUri() {
+    return String.format("%s:%s", this.ipAddress.getHostAddress(), this.port);
+  }
+}

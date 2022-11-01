f91b3ec1bdbf747993e46ecef9d6afc4ce12b065
GBFur <100243347+GBFur@users.noreply.github.com>
2022-11-01 13:50:42 +0100

    feat(frotend): #20 Scaffold frontend

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold">
+        anti<span className="text-amber-500">boom</span>
+      </h1>
+
+      <h2 className="font-bold">
+        <span>Temperature</span> reader
+      </h2>
+    </div>
+
+  );
+}
+
+export default App;

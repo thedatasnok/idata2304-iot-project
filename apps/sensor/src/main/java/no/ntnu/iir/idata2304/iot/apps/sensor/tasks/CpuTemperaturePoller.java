81b6919c57571c65995312d7184a29ae6a4b7623
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-11-01 12:46:24 +0100

    fix(sensor): get file uri from classloader instead of a relative path

--- apps/sensor/src/main/java/no/ntnu/iir/idata2304/iot/apps/sensor/tasks/CpuTemperaturePoller.java
+++ apps/sensor/src/main/java/no/ntnu/iir/idata2304/iot/apps/sensor/tasks/CpuTemperaturePoller.java
@@ -3,7 +3,6 @@ package no.ntnu.iir.idata2304.iot.apps.sensor.tasks;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,10 +18,14 @@ public class CpuTemperaturePoller {
     
   @Scheduled(fixedRate = 5000)
 	public void readTemperature() throws IOException {
-    // TODO: Read the correct file sys/class/thermal/thermal_zone0/temp
-		Path path = Paths.get("apps/sensor/src/main/resources/number.txt");
-    int temperature = Integer.valueOf(Files.readAllLines(path).get(0));
-    Float temperatureValue = (temperature / 1000f);
-    this.mqttGateway.sendToMqtt(temperatureValue.toString().getBytes());
+    try {
+      // TODO: Read the correct file sys/class/thermal/thermal_zone0/temp
+      var path = Path.of(this.getClass().getClassLoader().getResource("number.txt").toURI());
+      int temperature = Integer.parseInt(Files.readString(path));
+      Float temperatureValue = (temperature / 1000f);
+      this.mqttGateway.sendToMqtt(temperatureValue.toString().getBytes());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 	}
 }

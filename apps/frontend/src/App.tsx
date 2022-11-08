d9e6794ec197fc305ff8370aea93aa2f370d2e85
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-11-08 10:46:02 +0100

    feat(frontend): made the layout more responsive

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -14,18 +14,22 @@ import SensorStatusIndicatorHints from './components/indicator/SensorStatusIndic
 const App = () => {
   return (
     <div className='flex flex-col h-screen w-screen overflow-hidden'>
-      <h1 className='font-bold text-2xl text-center py-4'>
+      <h1 className='font-bold text-2xl text-center py-2 bg-zinc-800 border-b border-zinc-700 md:bg-transparent md:border-transparent'>
         <span className='text-white'>anti</span>
         <span className='text-amber-600'>boom</span>
       </h1>
+      
+      <a href='https://github.com/thedatasnok/idata2304-iot-project' className='fixed top-4 right-2 text-amber-600 font-semibold text-sm'>
+        github
+      </a>
 
-      <div className='flex-1 flex flex-row px-24 pb-24 overflow-hidden'>
-        <div className='w-96 px-4 py-2 overflow-hidden flex flex-col flex-shrink-0'>
+      <div className='flex-1 flex flex-col-reverse items-center overflow-y-auto px-4 md:flex-row md:items-stretch md:overflow-hidden md:px-8 lg:px-24 md:pb-24'>
+        <div className='w-full md:w-96 my-4 overflow-hidden flex flex-col flex-shrink-0'>
           <h2 className='font-semibold text-xl'>Sensors</h2>
 
           <SensorStatusIndicatorHints />
 
-          <div className='flex flex-col overflow-y-auto gap-2'>
+          <div className='flex flex-col overflow-y-auto gap-2 p-1'>
             {[...Array(30)].map((_, i) => (
               <Card
                 key={i + 1}
@@ -37,15 +41,15 @@ const App = () => {
           </div>
         </div>
 
-        <div className='flex-1'>
-          <ResponsiveContainer>
+        <div className='flex-1 w-full'>
+          <ResponsiveContainer width='99%'>
             <LineChart
               data={[]}
               margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
+                top: 65,
+                right: 0,
+                left: 0,
+                bottom: 0,
               }}
             >
               <CartesianGrid strokeDasharray='3 3' />

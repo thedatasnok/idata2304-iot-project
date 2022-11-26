f033a21a18e48aadbec164dd966139ba07e111be
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-11-26 18:01:56 +0100

    build: upgrade to spring boot 3

--- apps/ingest/src/main/java/no/ntnu/iir/idata2304/iot/apps/ingest/model/CpuTemperatureMeasurement.java
+++ apps/ingest/src/main/java/no/ntnu/iir/idata2304/iot/apps/ingest/model/CpuTemperatureMeasurement.java
@@ -1,8 +1,8 @@
 package no.ntnu.iir.idata2304.iot.apps.ingest.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;

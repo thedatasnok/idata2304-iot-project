01a1b759432b30b5be2abb855a901b523aaa6acc
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-12-03 00:10:47 +0100

    fix(frontend): sort color indicators

--- apps/frontend/src/components/indicator/SensorStatusIndicatorHints.tsx
+++ apps/frontend/src/components/indicator/SensorStatusIndicatorHints.tsx
@@ -48,7 +48,7 @@ export const convertTemperatureToColor = (temperature: number | undefined) => {
 const SensorStatusIndicatorHints = () => {
   return (
     <div className='flex flex-row justify-between mb-2'>
-      {Object.keys(SensorTemperatureColors).map((key) => (
+      {Object.keys(SensorTemperatureColors).sort().map((key) => (
         <div key={key} className='flex items-center gap-2'>
           <div
             className={clsx('w-2 h-2 rounded-full')}
